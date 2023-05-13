8ff1911f42efc57f03dc5d0c74e9c787da554623
Tirinox <developer@tirinox.ru>
2023-05-13 18:12:07 +0300

    Debug OKx balance

--- distribute_main.py
+++ distribute_main.py
@@ -1,7 +1,11 @@
 import argparse
+from pprint import pprint
+
+import ccxt
+import yaml
 
 
-def read_config_from_agrs():
+def read_arguments():
     arg_parser = argparse.ArgumentParser(description='Distribute coins')
     arg_parser.add_argument('--config', type=str, default='config.yaml', help='config file; see example.yaml')
     arg_parser.add_argument('--debug', action='store_true', help='debug mode')
@@ -9,9 +13,28 @@ def read_config_from_agrs():
     return arg_parser.parse_args()
 
 
+def create_exchange(config):
+    return ccxt.okx({
+        'apiKey': config['api_key'],
+        'secret': config['secret'],
+        "password": config['password'],
+        "options": {'defaultType': 'spot'},
+    })
+
+
+def read_config(path):
+    with open(path) as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
+
+
 def main():
-    args = read_config_from_agrs()
-    print(args)
+    args = read_arguments()
+
+    config = read_config(args.config)
+
+    okx = create_exchange(config['exchange']['okx'])
+    b = okx.fetch_balance()
+    pprint(b)
 
 
 if __name__ == '__main__':
